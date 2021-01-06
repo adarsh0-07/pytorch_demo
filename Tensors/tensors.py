f83213a71d078d08ba38ea33595a14c646b1bbb1
adarsh0-07 <adarsh.07@live.com>
2021-01-06 01:12:07 -0500

    tensors

---
+++ Tensors/tensors.py
@@ -0,0 +1,8 @@
+import torch
+
+# creating tensors
+print(torch.FloatTensor([1, 2, 3]))
+print(torch.IntTensor([1, 2, 3]))
+
+
+# 1D tensors
--- src/test (copy).py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-print(np.array([1, 2, 3]))
-
-print("this is a python test code")
-
-print("test git push")
